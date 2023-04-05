22048a9cff29972fe09e7241adf2071f93da84f8
Sameer Siddiqui <Sameer.Siddiqui@studentambassadors.com>
2023-04-05 16:43:55 +0500

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+var content=document.getElementById("menu");
+var nav = document.getElementById("ho");
+function hideMenu(){
+    content.style.right = "-200px";
+}
+function showMenu(){
+    content.style.right = "0px";
+}
